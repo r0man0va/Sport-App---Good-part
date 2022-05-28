353684c588be05658cd6a5dc28dccd0777f33393
Tanya <darkoracle1@mail.ru>
2022-05-28 13:49:53 +0300

    Избавилась от дублирования кода.

--- Sport App - Course work (good part)/SettingsForm.cs
+++ Sport App - Course work (good part)/SettingsForm.cs
@@ -18,36 +18,14 @@ namespace Sport_App___Course_work__good_part_
         {
             InitializeComponent();
         }
-
         private void ExerciseMinusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetExerciseTimeTextBox.Text;
-
-            Regex regex = new Regex(@"[\w]+");
-            MatchCollection matches = regex.Matches(currentSecondsCount);
-
-            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
-            int newSeconds = currentSeconds - 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            newSecondsCount += " сек";
-            SetExerciseTimeTextBox.Text = newSecondsCount;
-
+            ExerciseActionButton_Clicked(false);  // false для "минус"
         }
 
         private void ExercisePlusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetExerciseTimeTextBox.Text;
-
-            Regex regex = new Regex(@"[\w]+");
-            MatchCollection matches = regex.Matches(currentSecondsCount);
-
-            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
-            int newSeconds = currentSeconds + 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            newSecondsCount += " сек";
-            SetExerciseTimeTextBox.Text = newSecondsCount;
+            ExerciseActionButton_Clicked(true);  // true для "плюс"
         }
 
         private void SetExerciseTimeTextBox_TextChanged(object sender, EventArgs e)
@@ -57,32 +35,12 @@ namespace Sport_App___Course_work__good_part_
 
         private void RestMinusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetRestTimeTextBox.Text;
-
-            Regex regex = new Regex(@"[\w]+");
-            MatchCollection matches = regex.Matches(currentSecondsCount);
-
-            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
-            int newSeconds = currentSeconds - 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            newSecondsCount += " сек";
-            SetRestTimeTextBox.Text = newSecondsCount;
+            RestActionButton_Clicked(false);  // false для "минус"
         }
 
         private void RestPlusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetRestTimeTextBox.Text;
-
-            Regex regex = new Regex(@"[\w]+");
-            MatchCollection matches = regex.Matches(currentSecondsCount);
-
-            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
-            int newSeconds = currentSeconds + 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            newSecondsCount += " сек";
-            SetRestTimeTextBox.Text = newSecondsCount;
+            RestActionButton_Clicked(true);  // true для "плюс"
         }
 
         private void SetRestTimeTextBox_TextChanged(object sender, EventArgs e)
@@ -92,29 +50,87 @@ namespace Sport_App___Course_work__good_part_
 
         private void DurationMinusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetDurationCountTextBox.Text;
-            
-            int currentSeconds = Convert.ToInt32(currentSecondsCount);
-            int newSeconds = currentSeconds - 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            SetDurationCountTextBox.Text = newSecondsCount;
+            DurationActionButton_Clicked(false);
         }
 
         private void DurationPlusButton_Click(object sender, EventArgs e)
         {
-            string currentSecondsCount = SetDurationCountTextBox.Text;
-
-            int currentSeconds = Convert.ToInt32(currentSecondsCount);
-            int newSeconds = currentSeconds + 1;
-
-            string newSecondsCount = newSeconds.ToString();
-            SetDurationCountTextBox.Text = newSecondsCount;
+            DurationActionButton_Clicked(true);
         }
 
         private void SetDurationCountTextBox_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void ExerciseActionButton_Clicked(bool actionMode) 
+        {
+            string currentSecondsCount = SetExerciseTimeTextBox.Text;
+
+            Regex regex = new Regex(@"[\w]+");
+            MatchCollection matches = regex.Matches(currentSecondsCount);
+
+            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
+            int newSeconds;
+            if (actionMode) // случай нажатия "плюс" ExercisePlusButton_Click()
+            {
+                newSeconds = currentSeconds + 1;
+            }
+            else
+            {         // случай нажатия "минус" ExerciseMinusButton_Click()
+                newSeconds = currentSeconds - 1;
+            }
+            if (newSeconds < 0) newSeconds = 0;  // чтоб значения времени не ушли в минус
+            
+            string newSecondsCount = newSeconds.ToString();
+            newSecondsCount += " сек";
+            SetExerciseTimeTextBox.Text = newSecondsCount;
+        }
+
+        private void RestActionButton_Clicked(bool actionMode) 
+        {
+            string currentSecondsCount = SetRestTimeTextBox.Text;
+
+            Regex regex = new Regex(@"[\w]+");
+            MatchCollection matches = regex.Matches(currentSecondsCount);
+
+            int currentSeconds = Convert.ToInt32((string)matches[0].Value);
+
+            int newSeconds;
+            if (actionMode)  // случай нажатия "плюс" RestPlusButton_Click()
+            {
+                newSeconds = currentSeconds + 1;
+            }
+            else
+            {          // случай нажатия "минус" RestMinusButton_Click()
+                newSeconds = currentSeconds - 1;
+            }
+            if (newSeconds < 0) newSeconds = 0;  // чтоб значения времени не ушли в минус
+
+            string newSecondsCount = newSeconds.ToString();
+            newSecondsCount += " сек";
+            SetRestTimeTextBox.Text = newSecondsCount;
+        }
+
+        private void DurationActionButton_Clicked(bool actionMode) 
+        {
+            string currentSecondsCount = SetDurationCountTextBox.Text;
+
+            int currentSeconds = Convert.ToInt32(currentSecondsCount);
+            int newSeconds;
+
+            if (actionMode)  // случай нажатия "плюс" DurationPlusButton_Click()
+            {
+                newSeconds = currentSeconds + 1;
+            }
+            else
+            {              // случай нажатия "минус" DurationMinusButton_Click()
+                newSeconds = currentSeconds - 1;
+            }
+            if (newSeconds < 0) newSeconds = 0;  // чтоб значения времени не ушли в минус
+
+            string newSecondsCount = newSeconds.ToString();
+            SetDurationCountTextBox.Text = newSecondsCount;
         }
     }
 }
